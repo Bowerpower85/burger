d4a1256de6536302a57539d93ac280e116e01e61
Jonathan Bower <bowerpower85@gmail.com>
2020-11-30 14:43:27 -0800

    Added content to schema and seeds files.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers (burger_name, devoured) values ("Cheeseburger");
+insert into burgers (burger_name, devoured) values ("Mushroomburger");
+insert into burgers (burger_name, devoured) values ("A1 peppercorn burger");
