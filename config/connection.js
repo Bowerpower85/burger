6fc7f1e82e1d3ddc9f1bafa4116994086e3334bc
Jonathan Bower <bowerpower85@gmail.com>
2020-11-30 16:09:51 -0800

    setup my connection and orm js and set it up to launch to heroku.

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+const connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: '3306',
+        user: 'riw57yhptoksfp2f',
+        password: 'vibxmozchr3zaf1j',
+        database: 'burgers_db'
+    });
+};
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
